d3709e474e79cccadb3451928fe2cf39043301df
viettu2k <doanviettu2k@gmail.com>
2021-10-27 19:46:44 +0700

    adding routing pages

---
+++ src/Routes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import Home from "./core/Home";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" exact component={Signin} />
+        <Route path="/signup" exact component={Signup} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
