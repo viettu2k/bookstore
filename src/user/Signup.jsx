c5a8fb5013d277042e5ea25f51b87a8096f41798
viettu2k <doanviettu2k@gmail.com>
2021-10-28 09:04:54 +0700

    adding user signup

--- src/user/Signup.jsx
+++ src/user/Signup.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Layout from "../core/Layout";
-// import { API } from "../config";
+import { API } from "../config";
 
-export default function Signup() {
+const Signup = () => {
   const [values, setValues] = useState({
     name: "",
     email: "",
@@ -11,11 +11,35 @@ export default function Signup() {
     success: false,
   });
 
+  const { name, email, password } = values;
+
   const handleChange = (name) => (event) => {
     setValues({ ...values, error: false, [name]: event.target.value });
   };
 
-  const signupForm = () => (
+  const signup = (user) => {
+    fetch(`${API}/signup`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    signup({ name, email, password });
+  };
+
+  const signUpForm = () => (
     <form>
       <div className="form-group">
         <label className="text-muted">Name</label>
@@ -25,6 +49,7 @@ export default function Signup() {
           className="form-control"
         />
       </div>
+
       <div className="form-group">
         <label className="text-muted">Email</label>
         <input
@@ -33,6 +58,7 @@ export default function Signup() {
           className="form-control"
         />
       </div>
+
       <div className="form-group">
         <label className="text-muted">Password</label>
         <input
@@ -41,18 +67,22 @@ export default function Signup() {
           className="form-control"
         />
       </div>
-      <button className="btn btn-primary">Submit</button>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
 
   return (
     <Layout
       title="Signup"
-      description="Signup to Node Rect E-commerce App"
+      description="Signup to Node React E-commerce App"
       className="container col-md-8 offset-md-2"
     >
-      {signupForm()}
+      {signUpForm()}
       {JSON.stringify(values)}
     </Layout>
   );
-}
+};
+
+export default Signup;
