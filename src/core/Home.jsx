5594c259dc3c68d39faa9ee56a57b0ecc0766e38
viettu2k <doanviettu2k@gmail.com>
2021-11-11 15:34:27 +0700

    adding product by arrival and sell

--- src/core/Home.jsx
+++ src/core/Home.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
 
 export default function Home() {
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadProductsBySell = () => {
+    getProducts("sold").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySell(data);
+      }
+    });
+  };
+
+  const loadProductsByArrival = () => {
+    getProducts("createdAt").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
+
   return (
     <Layout title="Home Page" description="Node React E-commerce App">
-      ...
+      {JSON.stringify(productsByArrival)}
+      <hr />
+      {JSON.stringify(productsBySell)}
     </Layout>
   );
 }
