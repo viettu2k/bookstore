e23814a4c5c8411395109452602a319b7c71b588
viettu2k <doanviettu2k@gmail.com>
2021-11-25 21:22:37 +0700

    search message to users

--- src/core/Search.jsx
+++ src/core/Search.jsx
@@ -51,12 +51,24 @@ const Search = () => {
     setData({ ...data, [name]: event.target.value, searched: false });
   };
 
+  const searchMessage = (searched, results) => {
+    if (searched && results.length > 0) {
+      return `Found ${results.length} products`;
+    }
+    if (searched && results.length < 1) {
+      return `No products found`;
+    }
+  };
+
   const searchedProducts = (results = []) => {
     return (
-      <div className="row">
-        {results.map((product, i) => (
-          <Card key={i} product={product} />
-        ))}
+      <div>
+        <h2 className="mt-4 mb-4">{searchMessage(searched, results)}</h2>
+        <div className="row">
+          {results.map((product, i) => (
+            <Card key={i} product={product} />
+          ))}
+        </div>
       </div>
     );
   };
@@ -67,7 +79,7 @@ const Search = () => {
         <div className="input-group input-group-lg">
           <div className="input-group-prepend">
             <select className="btn mr-2" onChange={handleChange("category")}>
-              <option value="All">Pick Category</option>
+              <option value="All">All</option>
               {categories.map((c, i) => (
                 <option key={i} value={c._id}>
                   {c.name}
