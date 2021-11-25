b2bf800e07978553a47145642ced37786acd28ce
viettu2k <doanviettu2k@gmail.com>
2021-11-25 21:14:17 +0700

    updating search component

--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -2,55 +2,55 @@ import { API } from "../config";
 import queryString from "query-string";
 
 export const getProducts = (sortBy) => {
-  return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
-    method: "GET",
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .catch((err) => console.log(err));
+    return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
+            method: "GET",
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .catch((err) => console.log(err));
 };
 
 export const getCategories = () => {
-  return fetch(`${API}/categories`, {
-    method: "GET",
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .catch((err) => console.log(err));
+    return fetch(`${API}/categories`, {
+            method: "GET",
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .catch((err) => console.log(err));
 };
 
 export const getFilteredProducts = (skip, limit, filters = {}) => {
-  const data = {
-    limit,
-    skip,
-    filters,
-  };
-  return fetch(`${API}/products/by/search`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    const data = {
+        limit,
+        skip,
+        filters,
+    };
+    return fetch(`${API}/products/by/search`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 export const list = (params) => {
-  const query = queryString.stringify(params);
-  console.log("query", query);
-  return fetch(`${API}/products?${query}`, {
-    method: "GET",
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .catch((err) => console.log(err));
+    const query = queryString.stringify(params);
+    console.log("query", query);
+    return fetch(`${API}/products/search?${query}`, {
+            method: "GET",
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .catch((err) => console.log(err));
 };
