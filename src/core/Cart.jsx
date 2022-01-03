203b8868a1f52ade445529cd5fb210bf9df5a80c
viettu2k <doanviettu2k@gmail.com>
2022-01-03 08:34:52 +0700

    braintree setup frontend

--- src/core/Cart.jsx
+++ src/core/Cart.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import Layout from "./Layout";
+import { Link } from "react-router-dom";
 import { getCart } from "./cartHelpers";
 import Card from "./Card";
 import Checkout from "./Checkout";
 
 const Cart = () => {
   const [items, setItems] = useState([]);
+  // const [cartSize, setCartSize] = useState([]);
+  const [run, setRun] = useState(false);
 
   useEffect(() => {
+    console.log("MAX DEPTH ...");
     setItems(getCart());
-  }, [items]);
+  }, [run]);
 
   const showItems = (items) => {
     return (
@@ -24,6 +27,9 @@ const Cart = () => {
             showAddToCartButton={false}
             cartUpdate={true}
             showRemoveProductButton={true}
+            setRun={setRun}
+            run={run}
+            // changeCartSize={changeCartSize}
           />
         ))}
       </div>
@@ -32,23 +38,23 @@ const Cart = () => {
 
   const noItemsMessage = () => (
     <h2>
-      Your cart is empty. <br /> <Link to="/shop">Continue shopping</Link>
+      Your Cart is empty. <br />
+      <Link to="/shop"> Continue shopping. </Link>
     </h2>
   );
 
   return (
     <Layout
       title="Shopping Cart"
-      description="Manage your cart items. Add remove checkout or continue shopping."
+      description="Checkout now!"
       className="container-fluid"
     >
       <div className="row">
         <div className="col-6">
           {items.length > 0 ? showItems(items) : noItemsMessage()}
         </div>
-
         <div className="col-6">
-          <h2 className="mb-4">Your cart summary</h2>
+          <h2 className="mb-4">Your Cart Summary</h2>
           <hr />
           <Checkout products={items} />
         </div>
