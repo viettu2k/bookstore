dcb1619504e41b04f33654e7ac1156c368934cc1
viettu2k <doanviettu2k@gmail.com>
2022-01-03 16:46:14 +0700

    handling payment frontend

--- src/core/Cart.jsx
+++ src/core/Cart.jsx
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from "react";
-import Layout from "./Layout";
 import { Link } from "react-router-dom";
+import Layout from "./Layout";
 import { getCart } from "./cartHelpers";
 import Card from "./Card";
 import Checkout from "./Checkout";
 
 const Cart = () => {
   const [items, setItems] = useState([]);
-  // const [cartSize, setCartSize] = useState([]);
-  const [run, setRun] = useState(false);
 
   useEffect(() => {
-    console.log("MAX DEPTH ...");
     setItems(getCart());
-  }, [run]);
+  }, [items]);
 
   const showItems = (items) => {
     return (
@@ -27,9 +24,6 @@ const Cart = () => {
             showAddToCartButton={false}
             cartUpdate={true}
             showRemoveProductButton={true}
-            setRun={setRun}
-            run={run}
-            // changeCartSize={changeCartSize}
           />
         ))}
       </div>
@@ -38,23 +32,23 @@ const Cart = () => {
 
   const noItemsMessage = () => (
     <h2>
-      Your Cart is empty. <br />
-      <Link to="/shop"> Continue shopping. </Link>
+      Your cart is empty. <br /> <Link to="/shop">Continue shopping</Link>
     </h2>
   );
 
   return (
     <Layout
       title="Shopping Cart"
-      description="Checkout now!"
+      description="Manage your cart items. Add remove checkout or continue shopping."
       className="container-fluid"
     >
       <div className="row">
         <div className="col-6">
           {items.length > 0 ? showItems(items) : noItemsMessage()}
         </div>
+
         <div className="col-6">
-          <h2 className="mb-4">Your Cart Summary</h2>
+          <h2 className="mb-4">Your cart summary</h2>
           <hr />
           <Checkout products={items} />
         </div>
