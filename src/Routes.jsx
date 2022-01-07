2372fc95984396b6e876894e502c803097d8a82d
viettu2k <doanviettu2k@gmail.com>
2022-01-07 16:19:14 +0700

    user update profile methods

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,8 +4,8 @@ import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 import Home from "./core/Home";
 import PrivateRoute from "./auth/PrivateRoute";
-import AdminRoute from "./auth/AdminRoute";
 import Dashboard from "./user/UserDashboard";
+import AdminRoute from "./auth/AdminRoute";
 import AdminDashboard from "./user/AdminDashboard";
 import AddCategory from "./admin/AddCategory";
 import AddProduct from "./admin/AddProduct";
@@ -13,15 +13,16 @@ import Shop from "./core/Shop";
 import Product from "./core/Product";
 import Cart from "./core/Cart";
 import Orders from "./admin/Orders";
+import Profile from "./user/Profile";
 
-export default function Routes() {
+const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/shop" exact component={Shop} />
         <Route path="/signin" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
-        <Route path="/shop" exact component={Shop} />
         <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
         <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
         <AdminRoute path="/create/category" exact component={AddCategory} />
@@ -29,7 +30,10 @@ export default function Routes() {
         <Route path="/product/:productId" exact component={Product} />
         <Route path="/cart" exact component={Cart} />
         <AdminRoute path="/admin/orders" exact component={Orders} />
+        <PrivateRoute path="/profile/:userId" exact component={Profile} />
       </Switch>
     </BrowserRouter>
   );
-}
+};
+
+export default Routes;
